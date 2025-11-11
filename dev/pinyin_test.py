50959bd51df04838b98e99b9aed3e58fd5b919ca
ulisesrey <ulises.rey.torne@gmail.com>
2025-11-11 17:03:16 +0100

    trying to use the dict

--- dev/pinyin_test.py
+++ dev/pinyin_test.py
@@ -1,4 +1,7 @@
 from pypinyin import pinyin
+from pypinyin import Style
+from pypinyin.constants import PINYIN_DICT
 
+print(pinyin('吗', style=Style.TONE3, heteronym=True))
 
-print(pinyin('中心', heteronym=True))
+PINYIN_DICT
